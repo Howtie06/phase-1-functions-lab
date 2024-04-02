4441b766b87ab753068ab56a7cf9635657100d16
audrey cherop <audreycherop06@gmail.com>
2024-04-02 09:47:54 +0300

    index.js

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(someValue) {
+  const hq = 42; 
+  return Math.abs(someValue - hq); 
+}
+
+
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264; 
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  let fare = 0;
+
+  if (distance <= 400) {
+    fare = 0;
+  } else if (distance > 400 && distance <= 2000) {
+    fare = (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance < 2500) {
+    fare = 25;
+  } else if (distance >= 2500) {
+    return 'cannot travel that far'; 
   }
-  
-  
-  function distanceFromHqInFeet(someValue) {
-    const blocks = distanceFromHqInBlocks(someValue);
-    return blocks * 264; 
-  }
-  
-  
-  function distanceTravelledInFeet(start, destination) {
-    return Math.abs(destination - start) * 264;
-  }
-  
- 
-  function calculatesFarePrice(start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
-    if (distance <= 400) {
-      return 0; 
-    } else if (distance > 400 && distance <= 2000) {
-      return (distance - 400) * 0.02; 
-    } else if (distance > 2000 && distance < 2500) {
-      return 25; 
-    } else {
-      return 'cannot travel that far'; 
-  }
-  
-  module.exports = {
-    distanceFromHqInBlocks,
-    distanceFromHqInFeet,
-    distanceTravelledInFeet,
-    calculatesFarePrice
-  };
+
+  return fare;
+}
